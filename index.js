05494b61dbc68fff9df5c843ce0f9a243733db5d
Flavio Neubauer <flavio.neubauer@murah.com.br>
2019-11-21 13:26:10 -0300

    fix bugs

--- index.js
+++ index.js
@@ -4,35 +4,35 @@ const express = require('express');
 const cors = require('cors')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
 app.get('/validate', (req, res) => {
 	const qUrl = req.query.url;
 	if(qUrl){
-		console.log(`checking ${qURl}`);
+		console.log(`checking ${qUrl}`);
 		request({method: 'GET', uri: qUrl, redirect: true}, function (error, response, body) {
 			try{
 				const $ = cheerio.load(response.body);
 				const url = $("meta").attr().content.split(';')[1].split('=')[1].trim();
 				if(url){
-					console.log(`checking ${qURl}`);
+					console.log(`checking ${qUrl}`);
 					res.status(200);
 					res.send(url);
 				}else{
-					throw new Exception('url not found on meta tag');*-
+					throw new Error('url not found on meta tag');
 				}
 
 			}catch(e){
 				console.error(e);
 				res.status(500);
-				res.send(e);
+				res.send('Url not found on meta tag. ' + e.message);
 			}
 		});
 	}else{
 		res.status(400);
-		res.send('missing query url');
+		res.send('Url Utils usage: /validate?url=http://me.site.com:9292');
 	}
 });
 
